b7bff141acb37623e7077b2b36343272850c29b8
Shing Andy <chanshing935@gmail.com>
2023-01-29 16:54:30 +0800

    Make the turtle and rhino inheritance from catchanimal class, and developed remove arrow function in level 2

---
+++ Assets/Script/CatchBtnEvent.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class SavingBtnEvent : MonoBehaviour
+, IPointerDownHandler
+, IPointerUpHandler
+, IEventSystemHandler
+{
+    savingRhinoSystem savingRhinoSystem;
+    triggerRhino triggerRhino;
+
+    private void Start()
+    {
+        // find the catch turtle system script in the parent game object
+        savingRhinoSystem = gameObject.GetComponentInParent<savingRhinoSystem>();
+        // find the trigger turtle script
+        triggerRhino = FindObjectOfType<triggerRhino>();
+    }
+
+    //player start holding the btn
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        StartCoroutine(savingRhinoSystem.Catching());
+    }
+
+    //player stop holding the btn
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        savingRhinoSystem.setQuit();
+        triggerRhino.playerStopCatching();
+    }
+}
--- Assets/Script/turtle.cs
+++ Assets/Script/turtle.cs
@@ -3,42 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class turtle : MonoBehaviour
+public class turtle : CatchAnimal
 {
-    public GameObject circleUi;
-    private Image circle;
-    private Animator animator;
-
-    private void Start()
-    {
-        //get the circle ui
-        circle = circleUi.GetComponent<Image>();
-        animator = circleUi.GetComponent<Animator>();
-    }
-
-    public void nearPlayer(bool yes)
-    {
-        if(yes)
-            circleUi.SetActive(true);
-        else
-            circleUi.SetActive(false);
-    }
-
-    //catching animation
-    public void catching(bool yes)
-    {
-        if (yes)
-        {
-            animator.SetBool("catching", true);
-            circle.color = new Color32(255, 0, 0, 100);
-        }
-        else
-        {
-            animator.SetBool("catching", false);
-            circle.color = new Color32(32, 255, 0, 100);
-        }
-    }
-
     public void DestroyObject()
     {
         Destroy(gameObject);
