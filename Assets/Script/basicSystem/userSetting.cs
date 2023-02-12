106e3a988d0834603e2d0711774167284491355f
Shing Andy <chanshing935@gmail.com>
2023-02-12 15:03:28 +0800

    Developed the setting function with saving to all scene

--- Assets/Script/basicSystem/userSetting.cs
+++ Assets/Script/basicSystem/userSetting.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class userSetting : MonoBehaviour {
 
     GameObject[] soundObject;
+    private int muted;
+    public GameObject mutedBtn;
+    public GameObject unMutedBtn;
 
     void Start()
     {
+        muted = PlayerPrefs.GetInt("muted", 0);
         //find all gameobject with audio source
         soundObject = GameObject.FindGameObjectsWithTag("Sound");
+        if (muted == 1)
+        {
+            MuteSound();
+            unMutedBtn.SetActive(true);
+        }
+        else
+        {
+            mutedBtn.SetActive(true);
+        }
     }
 
     void Update()
@@ -22,7 +35,29 @@ public class userSetting : MonoBehaviour {
         //mute or unmute all audio source
         for(int i = 0; i< soundObject.Length; i++)
         {
-            soundObject[i].GetComponent<AudioSource>().mute = !soundObject[i].GetComponent<AudioSource>().mute;
+            soundObject[i].GetComponent<AudioSource>().mute = true;
         }
+        muted = 1;
+        PlayerPrefs.SetInt("muted", 1);
+    }
+
+    public void UnMuteSound()
+    {
+        //mute or unmute all audio source
+        for (int i = 0; i < soundObject.Length; i++)
+        {
+            soundObject[i].GetComponent<AudioSource>().mute = false;
+        }
+        muted = 0;
+        PlayerPrefs.SetInt("muted", 0);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
     }
 }
