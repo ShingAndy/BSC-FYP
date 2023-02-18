f521866a3ef9c41c682a2214625ba7337e4240d0
Shing Andy <chanshing935@gmail.com>
2023-02-18 17:29:44 +0800

    fix bugs

--- Assets/Script/basicSystem/EnterLevel.cs
+++ Assets/Script/basicSystem/EnterLevel.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class EnterLevel : MonoBehaviour
 {
     public int level;
+    public int requiredTaskState;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag != "Player")
             return;
-        loadingScene loadingScene = FindObjectOfType<loadingScene>();
-        loadingScene.LoadScene(level);
+        Player player = FindObjectOfType<Player>();
+        if(player.GetTastState() >= requiredTaskState)
+        {
+            loadingScene loadingScene = FindObjectOfType<loadingScene>();
+            loadingScene.LoadScene(level);
+        }
     }
 
 }
